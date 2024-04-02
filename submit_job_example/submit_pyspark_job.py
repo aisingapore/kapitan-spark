17e1c2b785ac83f4f4da85e909fdd0b7c33f3ed3
cheewei <second_comet@yahoo.com>
2024-04-02 10:08:16 +0800

    update docker image to 0.0.2

--- submit_job_example/submit_pyspark_job.py
+++ submit_job_example/submit_pyspark_job.py
@@ -15,8 +15,7 @@ payload = {
   "driverCores": 1,
   "driverMemory": "1G",
   "conf": {
-    # "spark.kubernetes.container.image": 'ghcr.io/aisingapore/kapitan-spark/spark:0.0.1-spark3.5.0',
-    "spark.kubernetes.container.image": '<docker-image>/spark',
+    "spark.kubernetes.container.image": 'ghcr.io/aisingapore/kapitan-spark/spark:0.0.2-spark3.5.0',
     "spark.kubernetes.container.image.pullPolicy": 'Always',
     "spark.kubernetes.file.upload.path": 'local:///temp'
   }
