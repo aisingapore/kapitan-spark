afb4b078209e38bc3087df1b050a090b88d4bd53
cheewei <second_comet@yahoo.com>
2024-04-09 20:10:39 +0800

    fix jupyter notebook issues with jupytext

---
+++ installer/charts/jupyterlab/start-notebook.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Shim to emit warning and call start-notebook.py
+echo "WARNING: Use start-notebook.py instead"
+
+conda install sparkmagic -y
+pip install -r /tmp/requirements.txt  --no-cache-dir
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+jupyter-kernelspec install --user $(pip show sparkmagic | grep Location | cut -d" " -f2)/sparkmagic/kernels/pysparkkernel
+jupyter serverextension enable --py sparkmagic
+jupyter kernelspec uninstall sparkkernel -y
+jupyter kernelspec uninstall sparkrkernel -y
+jupyter kernelspec uninstall python3 -y
+mkdir -p /home/jovyan/.sparkmagic/
+cp /tmp/sparkmagic.json /home/jovyan/.sparkmagic/config.json
+cp /tmp/quickstart.ipynb /home/jovyan/work/quickstart.ipynb
+
+exec /usr/local/bin/start-notebook.py "$@"
